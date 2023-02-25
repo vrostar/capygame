574ebf9fb17347f11491c2ba9b4f7ef0c091fb49
Sasha Salmon <89836816+vrostar@users.noreply.github.com>
2023-02-25 22:18:23 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,65 @@
+
+const video = document.getElementById("webcam");
+const label = document.getElementById("label");
+const classifyBtn = document.querySelector("#Classify");
+const infoBtn = document.querySelector("#Info");
+
+// Speech synthesis to give instructions
+let synth = window.speechSynthesis
+
+function speak(text) {
+    if (synth.speaking) {
+        console.log('still speaking...')
+        return
+    }
+    if (text !== '') {
+        let utterThis = new SpeechSynthesisUtterance(text)
+        synth.speak(utterThis)
+    }
+}
+
+speak("Welcome to capybara game")
+
+classifyBtn.addEventListener("click", () => classify() + console.log("classify"));
+infoBtn.addEventListener("click", () => speak("Make sure you have a Capybara in the video frame and press the Lets Go button."));
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+            video.srcObject = stream;
+        })
+        .catch((err) => {
+            console.log("Something went wrong!");
+        });
+}
+
+// Extract the already learned features from MobileNet
+const featureExtractor = ml5.featureExtractor('MobileNet', modelLoaded);
+
+// Load the model
+function modelLoaded() {
+    featureExtractor.load('model/model.json');
+    console.log('Loaded the model succesfully')
+}
+
+// Create a new classifier using those features and with a video element
+const classifier = featureExtractor.classification(video, videoReady);
+
+// Triggers when the video is ready
+function videoReady() {
+    console.log('The video is ready!');
+}
+
+// Get a prediction for the image
+function classify() {
+    classifier.classify(document.getElementById('webcam'), (err, result) => {
+        console.log(result); // Should output 'ugly' or 'capy'
+        label.innerText = result[0].label;
+        if (result[0].label == "capy") {
+            speak("omg thats a spicy capy yippee!!!")
+        } else {
+            speak("thats a ugly bara, not good ew")
+        }
+    });
+}
